5843838e61e9d8187f06d438b00b1c18aa4ef08f
Elizabeth Gholson <eliz.gholson.9@gmail.com>
2018-06-13 23:24:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments){
+ musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
+ instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums'];
+   for(let i= 0; i < 3; i++){
+     return '${musicians} plays ${instruments}';
+   }
+}
+
+
+
+function johnLennonFacts(array){
+  let i = 0;
+   array = ['He was the last Beatle to learn to drive', 'He was never a vegetarian', 'He was a choir boy and a boy scout', 'He hated the sound of his own voice'];
+  while(i >= 3)
+  
+  return array.push(i-- + '!!!');
+}
+
+function iLoveTheBeatles(n){
+  var love = [];
+  do{
+    love.push("I love the Beatles!");
+  }
+  while()
+}
